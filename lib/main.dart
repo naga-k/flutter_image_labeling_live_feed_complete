240312cde0e4e7b78688c5ccaabf5d2418028d9c
Naga Karumuri <anjaneyulun02@gmail.com>
2022-06-06 17:04:18 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
-
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
 
-
-
-List<CameraDescription> cameras;
+late List<CameraDescription> cameras;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras();
@@ -15,17 +12,17 @@ Future<void> main() async {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  CameraImage img;
-  CameraController controller;
+  CameraImage? img;
+  CameraController? controller;
   bool isBusy = false;
   String result = "";
-  ImageLabeler imageLabeler;
+  late ImageLabeler imageLabeler;
 
   @override
   void initState() {
@@ -34,20 +31,15 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   //Initialize camera
-  initializeCamera () async
-  {
+  initializeCamera() async {
     controller = CameraController(cameras[0], ResolutionPreset.max);
-    await controller.initialize().then((_) {
+    await controller!.initialize().then((_) {
       if (!mounted) {
         return;
       }
-      controller.startImageStream((image) => {
-        if(!isBusy){
-          isBusy = true,
-          img = image,
-          doImageLabeling()
-        }
-      });
+      controller!.startImageStream((image) => {
+            if (!isBusy) {isBusy = true, img = image, doImageLabeling()}
+          });
     });
   }
 
@@ -57,9 +49,8 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-
   //Write image labeling code
-  doImageLabeling() async{
+  doImageLabeling() async {
     result = "";
     InputImage inputImg = getInputImage();
     final List<ImageLabel> labels = await imageLabeler.processImage(inputImg);
@@ -67,35 +58,33 @@ class _MyHomePageState extends State<MyHomePage> {
       final String text = label.label;
       final int index = label.index;
       final double confidence = label.confidence;
-      result+=text+"   "+confidence.toStringAsFixed(2)+"\n";
+      result += text + "   " + confidence.toStringAsFixed(2) + "\n";
     }
     setState(() {
-       result;
-       isBusy = false;
+      result;
+      isBusy = false;
     });
-
   }
 
-  InputImage getInputImage(){
-
+  InputImage getInputImage() {
     final WriteBuffer allBytes = WriteBuffer();
-    for (var plane in img.planes) {
+    for (var plane in img!.planes) {
       allBytes.putUint8List(plane.bytes);
     }
     final bytes = allBytes.done().buffer.asUint8List();
 
-    final Size imageSize = Size(img.width.toDouble(), img.height.toDouble());
+    final Size imageSize = Size(img!.width.toDouble(), img!.height.toDouble());
 
     final InputImageRotation imageRotation =
-        InputImageRotationMethods.fromRawValue(cameras[0].sensorOrientation) ??
-            InputImageRotation.Rotation_0deg;
+        InputImageRotationValue.fromRawValue(cameras[0].sensorOrientation) ??
+            InputImageRotation.rotation0deg;
 
     final InputImageFormat inputImageFormat =
-        InputImageFormatMethods.fromRawValue(img.format.raw) ??
-            InputImageFormat.NV21;
+        InputImageFormatValue.fromRawValue(img!.format.raw) ??
+            InputImageFormat.nv21;
 
-    final planeData = img.planes.map(
-          (var plane) {
+    final planeData = img!.planes.map(
+      (var plane) {
         return InputImagePlaneMetadata(
           bytesPerRow: plane.bytesPerRow,
           height: plane.height,
@@ -111,13 +100,13 @@ class _MyHomePageState extends State<MyHomePage> {
       planeData: planeData,
     );
 
-    final inputImage = InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
+    final inputImage =
+        InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
     return inputImage;
   }
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
@@ -134,7 +123,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: Container(
                           margin: EdgeInsets.only(top: 100),
                           height: 220,
-                          width : 320,
+                          width: 320,
                           child: Image.asset('images/lcd2.jpg')),
                     ),
                     Center(
@@ -143,20 +132,22 @@ class _MyHomePageState extends State<MyHomePage> {
                           margin: EdgeInsets.only(top: 118),
                           height: 177,
                           width: 310,
-                          child: img == null?Container(
-                            width: 140,
-                            height: 150,
-                            child: Icon(
-                              Icons.videocam,
-                              color: Colors.white,
-                            ),
-                          ):AspectRatio(
-                            aspectRatio: controller.value.aspectRatio,
-                            child: CameraPreview(controller),
-                          ),
+                          child: img == null
+                              ? Container(
+                                  width: 140,
+                                  height: 150,
+                                  child: Icon(
+                                    Icons.videocam,
+                                    color: Colors.white,
+                                  ),
+                                )
+                              : AspectRatio(
+                                  aspectRatio: controller!.value.aspectRatio,
+                                  child: CameraPreview(controller!),
+                                ),
                         ),
-                        onPressed: (){
-                            initializeCamera();
+                        onPressed: () {
+                          initializeCamera();
                         },
                       ),
                     ),
@@ -167,13 +158,13 @@ class _MyHomePageState extends State<MyHomePage> {
                     height: 245,
                     child: SingleChildScrollView(
                         child: Text(
-                          '$result',
-                          style: TextStyle(
-                              fontSize: 25,
-                              color: Colors.black,
-                              fontFamily: 'finger_paint'),
-                          textAlign: TextAlign.center,
-                        )),
+                      '$result',
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.black,
+                          fontFamily: 'finger_paint'),
+                      textAlign: TextAlign.center,
+                    )),
                   ),
                 ),
               ],
